47e45ef5b866022bc39d782eabea2e4d517448f1
inforhustle-hash <inforhustle@gmail.com>
2025-12-11 15:41:52 -0700

    Delete hustlehub_backend_FULL_RESTORE_v3 (1) directory

--- hustlehub_backend_FULL_RESTORE_v3 (1)/config/db.js
+++
@@ -1,8 +0,0 @@
-
-import mongoose from 'mongoose';
-export default async function connectDB(){
-  try{
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
-  }catch(e){console.error(e);}
-}
