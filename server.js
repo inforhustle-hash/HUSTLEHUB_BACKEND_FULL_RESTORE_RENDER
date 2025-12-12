bbc8ef68e944eb8e8373399bf352b2f8eee1ced7
inforhustle-hash <inforhustle@gmail.com>
2025-12-12 15:30:53 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
 import authRoutes from "./routes/authRoutes.js";
+import contactRoutes from "./routes/contactRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// 🔥 THIS IS THE MISSING LINK
 app.use("/api/auth", authRoutes);
+app.use("/api/contacts", contactRoutes);
+
+app.get("/", (req, res) => {
+  res.send("HustleHub Backend v3");
+});
+
+export default app;
